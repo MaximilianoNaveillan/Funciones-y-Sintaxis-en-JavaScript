31e7d1fd452ac50b38b64c89c272e6f56cf53a0d
elfercape <elfercape@gmail.com>
2026-02-05 22:07:06 -0300

    function_anonima 05-02-2026

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const ingresoBruto = document.getElementById("ingresoBruto")
-const IvaReturn = document.getElementById("IvaReturn")
+const ingresoBruto = document.getElementById('ingresoBruto');
+const IvaReturn = document.getElementById('IvaReturn');
 
-function iva(){
-const valorIngresado= ingresoBruto.value;
-let ivaValor = valorIngresado * 0.19;
-IvaReturn.value = ivaValor
-/* console.log(valorIngresado) */
+function iva() {
+  const valorIngresado = ingresoBruto.value;
+  let ivaValor = valorIngresado * 0.19;
+  IvaReturn.value = '.';
+  setTimeout(function () {
+    IvaReturn.value = '..';
+  }, 1000);
+  setTimeout(function () {
+    IvaReturn.value = '...';
+  }, 2000);
+  setTimeout(function () {
+    IvaReturn.value = ivaValor;
+  }, 3000);
+  /* console.log(valorIngresado) */
 }
-
